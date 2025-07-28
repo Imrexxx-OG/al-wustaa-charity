eab2fa5ced456641813ebceb7185b04c84064a27
Imrexxx-OG <imrexxx93@gmail.com>
2025-07-28 07:06:28 +0100

    added links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ function Footer() {
     <footer className="footer">
       <p>Al-Wustaa Charitable Foundation</p>
       <p>SAVE LIFE SAVE HUMANITY - ومن أحياها فكأنما أحيا الناس جميعا</p>
-      <p>Contact us: alwustaacharitablefoundation3@gmail.com | Follow us on <a href="https://t.me/your-telegram-link" target="_blank" rel="noopener noreferrer">Telegram</a></p>
+      <p>Contact us: alwustaacharitablefoundation3@gmail.com | Follow us on <a href="https://x.com/AlwustaaFound_n" target="_blank" rel="noopener noreferrer">X</a><a href="https://www.instagram.com/alwustaacharitablefoundation" target="_blank" rel="noopener noreferrer">Instagram</a>
+      <a href="https://www.youtube.com/@AL-WUSTAACHARITABLEFOUNDATION" target="_blank" rel="noopener noreferrer">YouTube</a><a href=" https://www.tiktok.com/@alwustaafoundation?lang=en" target="_blank" rel="noopener noreferrer">Tiktok</a></p>
+      
     </footer>
   );
 }
