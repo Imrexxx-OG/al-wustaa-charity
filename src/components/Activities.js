721f6eca26876563dd415e453d1e0514723f702d
Imrexxx-OG <imrexxx93@gmail.com>
2025-11-15 15:21:33 +0100

    i did some changes again

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -8,25 +8,25 @@ function Activities() {
         <div className="activity-card">
           <img src="/image1.jpg" alt="Activity 1" />
           <p>
-            2021 medical outreach at then arrahmatul-a'lamiya and now Al-wustaa charitable foundation
+            2021 Medical Outreach at then Arrahmatul-a'lamiya and now Al-wustaa charitable foundation
           </p>
         </div>
         <div className="activity-card">
           <img src="/image2.jpg" alt="Activity 2" />
           <p>
-            2022 medical outreach at then arrahmatul-a'lamiya and now Al-wustaa charitable foundation
+            2022 Medical Outreach at then Arrahmatul-a'lamiya and now Al-wustaa charitable foundation
           </p>
         </div>
         <div className="activity-card">
           <img src="/image3.jpg" alt="Activity 3" />
           <p>
-            2023 medical outreach at then arrahmatul-a'lamiya and now Al-wustaa charitable foundation
+            2023 Medical Outreach at then Arrahmatul-a'lamiya and now Al-wustaa charitable foundation
           </p>
         </div>
         <div className="activity-card">
           <img src="/image4.jpg" alt="Activity 4" />
           <p>
-            2024 medical outreach at then arrahmatul-a'lamiya and now Al-wustaa charitable foundation
+            2024 Medical Outreach at then Arrahmatul-a'lamiya and now Al-wustaa charitable foundation
           </p>
         </div>
       </div>
